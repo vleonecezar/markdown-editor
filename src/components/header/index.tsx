fd5ca63d0bd9e3b3151402aab1fc62c4580ca6b4
vleonecezar <vleone.social@gmail.com>
2023-01-18 04:35:52 -0300

    feat(header): base of file count, file name and button styled components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,15 @@
-import HeaderContainer from './styled';
+import { FileCount, FileName, HeaderContainer, SaveButton } from './styled';
 
 function Header() {
-  return <HeaderContainer>Header</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <FileCount>Total Files: 0</FileCount>
+      <div>
+        <FileName>File Name</FileName>
+        <SaveButton>Save</SaveButton>
+      </div>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
--- src/components/header/styled.ts
+++ src/components/header/styled.ts
@@ -1,11 +1,28 @@
 import styled from 'styled-components';
 
-const HeaderContainer = styled.header`
+export const HeaderContainer = styled.header`
   background-color: var(--primary-color);
   height: 50px;
   color: var(--text-color);
   display: flex;
   align-items: center;
+
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 `;
 
-export default HeaderContainer;
+export const FileCount = styled.span`
+  min-width: 250px;
+  text-align: center;
+  color: var(--text-color);
+`;
+
+export const FileName = styled.span`
+  margin-right: 30px;
+  color: var(--text-color);
+`;
+
+export const SaveButton = styled.button``;
