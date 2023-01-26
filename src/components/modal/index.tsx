d48d57f82dd7305567f7c8f67f1beb70316df99e
vleonecezar <vleone.social@gmail.com>
2023-01-25 23:51:56 -0300

    feat(modal): created the logic to create new file and to navigate to editor to edit it by clicking on create button

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable jsx-a11y/no-autofocus */
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useFile } from '../../context';
+import { Action, File } from '../../types';
+import getUniqueId from '../../utils/getUniqueId';
 import Button from '../button';
 import { Background, DiscardButton, ModalContainer } from './styled';
 
 type SetIsModalOpen = React.Dispatch<React.SetStateAction<boolean>>;
 
 type Event = React.MouseEvent<HTMLDivElement, MouseEvent>;
+
+interface CreateFileProps {
+  state: File[];
+  title: string;
+  dispatch: React.Dispatch<Action>;
+  navigate: (path: string) => void;
+}
 
 interface Props {
   setIsModalOpen: SetIsModalOpen;
@@ -22,7 +35,29 @@ const closeModalByClickOnButton = (setIsModalOpen: SetIsModalOpen) => {
   setIsModalOpen(false);
 };
 
+const createFile = (props: CreateFileProps) => {
+  const { state, title, dispatch, navigate } = props;
+  if (title && title.length <= 30) {
+    const id = getUniqueId(state);
+    dispatch({ type: 'CREATE_FILE', payload: { id, title } });
+    navigate(`/editor/${id}`);
+  } else {
+    alert('Please, enter the file name.'); /* temporary */
+  }
+};
+
 function Modal({ setIsModalOpen }: Props) {
+  const { state, dispatch } = useFile();
+  const [title, setTitle] = useState('');
+  const navigate = useNavigate();
+
+  const createFileProps = {
+    state,
+    title,
+    dispatch,
+    navigate,
+  };
+
   return (
     <Background
       onClick={(event) => closeModalByClickOnBackground(event, setIsModalOpen)}
@@ -30,7 +65,13 @@ function Modal({ setIsModalOpen }: Props) {
       <ModalContainer>
         <label htmlFor="title">
           File Name
-          <input type="text" />
+          <input
+            type="text"
+            value={title}
+            autoFocus
+            maxLength={30}
+            onChange={({ target }) => setTitle(target.value)}
+          />
         </label>
         <div>
           <DiscardButton
@@ -39,7 +80,9 @@ function Modal({ setIsModalOpen }: Props) {
           >
             Discard
           </DiscardButton>
-          <Button type="button">Create</Button>
+          <Button type="button" onClick={() => createFile(createFileProps)}>
+            Create
+          </Button>
         </div>
       </ModalContainer>
     </Background>
